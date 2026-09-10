9ae294a3ff3c16d0103f194c40a2b7bd7bf698e3
its-Kobi <kobi@krecorder.local>
2026-09-10 17:05:09 +0100

    fix: overlay as separate Win32 window (not in video, no hide/show lag)

--- src/Overlay.cpp
+++ src/Overlay.cpp
@@ -2,21 +2,63 @@
 #include "Settings.hpp"
 using namespace geode::prelude;
 RecordingOverlay& RecordingOverlay::get(){ static RecordingOverlay i; return i; }
-void RecordingOverlay::show(){
-    if(m_visible) {
-        if(m_dot) m_dot->setVisible(true);
-        return;
+
+#ifdef _WIN32
+void RecordingOverlay::createWinOverlay(){
+    if(m_overlayWnd) return;
+    auto hwndGD = FindWindowA(nullptr, "Geometry Dash");
+    if(!hwndGD) hwndGD = FindWindowA("GLFW30", nullptr);
+    if(!hwndGD) return;
+    RECT rc{}; GetWindowRect(hwndGD, &rc);
+    RECT cr{}; GetClientRect(hwndGD, &cr);
+    POINT pt{0,0}; ClientToScreen(hwndGD, &pt);
+    int winW = cr.right; int winH = cr.bottom;
+    float sz=10.f; float m=12.f;
+    std::string pos = KRecorderSettings::getOverlayPosition();
+    int x = pt.x + winW - (int)(m + sz/2);
+    int y = pt.y + (int)(m + sz/2);
+    if(pos=="top-left"){ x = pt.x + (int)m; y = pt.y + (int)m; }
+    else if(pos=="bottom-left"){ x = pt.x + (int)m; y = pt.y + winH - (int)(m+sz); }
+    else if(pos=="bottom-right"){ x = pt.x + winW - (int)(m+sz); y = pt.y + winH - (int)(m+sz); }
+    else { x = pt.x + winW - (int)(m+sz); y = pt.y + (int)m; } // top-right default
+
+    WNDCLASSA wc{}; wc.lpfnWndProc = DefWindowProcA;
+    wc.hInstance = GetModuleHandleA(nullptr);
+    wc.lpszClassName = "KRecorderOverlayCls";
+    RegisterClassA(&wc);
+    m_overlayWnd = CreateWindowExA(WS_EX_TOPMOST | WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW | WS_EX_NOACTIVATE,
+        "KRecorderOverlayCls", nullptr, WS_POPUP,
+        x, y, (int)sz, (int)sz, nullptr, nullptr, wc.hInstance, nullptr);
+    if(m_overlayWnd){
+        SetLayeredWindowAttributes(m_overlayWnd, 0, 255, LWA_ALPHA);
+        HBRUSH br = CreateSolidBrush(RGB(255,0,0));
+        HDC hdc = GetDC(m_overlayWnd);
+        RECT r{0,0,(int)sz,(int)sz};
+        FillRect(hdc, &r, br);
+        DeleteObject(br);
+        ReleaseDC(m_overlayWnd, hdc);
+        ShowWindow(m_overlayWnd, SW_SHOWNA);
+        UpdateWindow(m_overlayWnd);
     }
+}
+void RecordingOverlay::destroyWinOverlay(){
+    if(m_overlayWnd){ DestroyWindow(m_overlayWnd); m_overlayWnd=nullptr; }
+}
+#endif
+
+void RecordingOverlay::show(){
+    if(m_visible) return;
     if(!KRecorderSettings::isOverlayEnabled()) return;
+#ifdef _WIN32
+    // Use separate window so it is NOT captured by window DC BitBlt (game-only)
+    createWinOverlay();
+    m_visible = m_overlayWnd != nullptr;
+    return;
+#else
     auto dir = cocos2d::CCDirector::sharedDirector();
     auto scene = dir->getRunningScene();
     if(!scene) return;
-    // Reuse existing dot if already created
-    if(m_dot){
-        m_dot->setVisible(true);
-        m_visible=true;
-        return;
-    }
+    if(m_dot){ m_dot->setVisible(true); m_visible=true; return; }
     auto win = dir->getWinSize();
     float sz=10.f; float m=12.f;
     std::string pos = KRecorderSettings::getOverlayPosition();
@@ -31,9 +73,15 @@ void RecordingOverlay::show(){
     m_dot->setID("krecorder-indicator");
     scene->addChild(m_dot);
     m_visible=true;
+#endif
 }
 void RecordingOverlay::hide(){
     if(!m_visible) return;
+#ifdef _WIN32
+    destroyWinOverlay();
+    m_visible=false;
+#else
     if(m_dot) m_dot->setVisible(false);
     m_visible=false;
+#endif
 }
--- src/Overlay.hpp
+++ src/Overlay.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include <Geode/Geode.hpp>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 class RecordingOverlay {
 public:
     static RecordingOverlay& get();
@@ -9,4 +12,9 @@ public:
 private:
     bool m_visible=false;
     cocos2d::CCLayerColor* m_dot=nullptr;
+#ifdef _WIN32
+    HWND m_overlayWnd=nullptr;
+    void createWinOverlay();
+    void destroyWinOverlay();
+#endif
 };
